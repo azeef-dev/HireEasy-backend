6fcb90b9af8c480ef33049a76d841dbed9c11ca6
azeefashraf22sketch <azeefashraf22@gmail.com>
2026-09-04 04:39:15 -0700

    add forgotPassword and resetPassword routes to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/authController');
+const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
-const { validate, registerValidation, loginValidation } = require('../middleware/validators');
+const {
+    validate,
+    registerValidation,
+    loginValidation,
+    forgotPasswordValidation,
+    resetPasswordValidation,
+} = require('../middleware/validators');
 
 const router = express.Router();
 
 router.post('/register', registerValidation, validate, register);
 router.post('/login', loginValidation, validate, login);
 router.get('/me', protect, getMe);
+router.post('/forgot-password', forgotPasswordValidation, validate, forgotPassword);
+router.post('/reset-password/:token', resetPasswordValidation, validate, resetPassword);
 
 module.exports = router;
